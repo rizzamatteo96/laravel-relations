8ba386a81566aff8d236ac90edac206510b95a50
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-09-15 19:41:30 +0200

    add delete command to category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -17,7 +17,13 @@
         <td>
           <a href="{{route('admin.categories.show', $category->id)}}" class="btn btn-outline-info"><i class="far fa-clipboard"></i></a>
           <a href="{{route('admin.categories.edit', $category->id)}}" class="btn btn-outline-secondary"><i class="far fa-edit"></i></a>
-          <a href="#" class="btn btn-outline-danger"><i class="far fa-trash-alt"></i></a>
+          {{-- <a href="#" class="btn btn-outline-danger"><i class="far fa-trash-alt"></i></a> --}}
+          <form action="{{route('admin.categories.destroy', $category->id)}}" method="POST" class="d-inline-block">
+            {{-- Per ogni form bisogna inserire il token altrimenti il cambiamento non viene accettato dal sistema --}}
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-outline-danger" onclick="return confirm('Sei sicuro di voler cancellare la categoria?')"><i class="far fa-trash-alt"></i></button>
+          </form>
         </td>
       </tr>
     @endforeach

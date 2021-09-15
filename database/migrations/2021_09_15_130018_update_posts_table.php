e640fe9af2d3465f1c01671b0755fa73ee4e25b5
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-09-15 15:08:35 +0200

    add new foreign key on table posts

---
+++ database/migrations/2021_09_15_130018_update_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id')->after('title')->nullable();
+
+            $table->foreign('category_id')
+                  ->references('id')
+                  ->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // in caso di rollback cancello la proprietà di foreign_key alla colonna category_id 
+            $table->dropForeign('posts_category_id_foreign');
+            // successivamente cancello la colonna category_id
+            $table->dropColumn('category_id');
+        });
+    }
+}
